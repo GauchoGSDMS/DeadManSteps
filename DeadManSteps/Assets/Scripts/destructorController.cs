39544c68b5ca3d434c9e337dd5ddf1775a7159cb
Federico Sanches <federicohernansanches@gmail.com>
2018-07-04 16:08:04 -0300

    First commit

---
+++ DeadManSteps/Assets/Scripts/destructorController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destructorController : MonoBehaviour {
+
+    void OnCollisionEnter (Collision collision)
+    {
+        Destroy(collision.gameObject);
+        Debug.Break();
+    }
+}
