8e354419420fb3bc5c871870e3d85ba36f981de4
Federico Sanches <federicohernansanches@gmail.com>
2018-08-28 13:35:03 -0300

    Dialogos
    Primer intento funcional de dialogos

--- DeadManSteps/Assets/pickUpItems.cs
+++ DeadManSteps/Assets/pickUpItems.cs
@@ -2,17 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class pickUpItems : MonoBehaviour 
-{
+public class pickUpItems : MonoBehaviour {
+
+	private DialogueTrigger script;
+
+	private void Start() {
+		script = this.GetComponent<DialogueTrigger>();
+	}
 
 	private void OnTriggerStay(Collider other)
 	{
 		if (other.tag == "Player") 
 		{
-			if (Input.GetKey (KeyCode.E)) 
-			{
-				
-				Destroy (this.gameObject);	
+			if (Input.GetKey (KeyCode.E)) {
+				script.TriggerDialogue();
 			} 	
 		} 
 	}
