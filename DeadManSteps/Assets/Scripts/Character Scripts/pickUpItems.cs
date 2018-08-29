e03730c251479514a7b6982d2f319adf777ce224
Lucas <lucas.galetti1991@gmail.com>
2018-08-28 22:10:51 -0300

    a
    a

---
+++ DeadManSteps/Assets/Scripts/Character Scripts/pickUpItems.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickUpItems : MonoBehaviour 
+{
+
+	private void OnTriggerStay(Collider other)
+	{
+		if (other.tag == "Player") 
+		{
+			if (Input.GetKey (KeyCode.E)) 
+			{
+				
+				Destroy (this.gameObject);	
+			} 	
+		} 
+	}
+}
