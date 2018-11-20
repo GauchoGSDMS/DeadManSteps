958866d7dd2feee9e98c31da5424e70ce3ab5f5c
martingaleano <galeanomarting@gmail.com>
2018-11-20 19:59:43 -0300

    Inventario - Commit inicial

---
+++ DeadManSteps/Assets/Scripts/InventarioCanvas.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventarioCanvas : MonoBehaviour {
+
+    public GameObject Inventario;
+
+		// void Start ()
+  //   {
+  //   	Inventario.SetActive(0);
+  //   }
+
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            Inventario.SetActive(!Inventario.activeInHierarchy);
+        }
+	}
+}
