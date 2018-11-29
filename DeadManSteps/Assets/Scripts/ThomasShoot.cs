f980e41226a370e76d48d2616e07e84dfe89e946
Diego Colussi <diegomartin010@gmail.com>
2018-11-29 00:56:43 -0300

    Apuntar y mostrar Pistola. Disparar forward
    Pendiente Cooldown, y contar disparos.
    Animacion de Disparo y alguna otra cosa.
    Camara esa bugeada.

--- DeadManSteps/Assets/Scripts/ThomasShoot.cs
+++ DeadManSteps/Assets/Scripts/ThomasShoot.cs
@@ -5,27 +5,48 @@ using UnityEngine;
 
 public class ThomasShoot : MonoBehaviour
 {
+	int _cantDisparos = 5;
+	public GameObject pistola;
+
     void Update()
-    {
-        //Layer Mask no borrar porque sino no anda
+    {    
+    	pistola.SetActive(false);
+        Animator m_Animator = GetComponent<Animator>();
+		//Clickeando mouse apuntar.
+		if (Input.GetMouseButton(1)){
+			m_Animator.Play("PistolAim");
+			pistola.SetActive(true);
+			Disparar();
+		}
+
+		//Si levantamos click derecho vuelve a idle.
+		if (Input.GetMouseButtonUp(1)){
+			m_Animator.Play("Idle");
+		}
+
+		
+		
+    }
+
+    //Metodo para disparar.
+    void Disparar(){
+    	//Layer Mask no borrar porque sino no anda
         int layerMask = 1 << 8;
         //No borrar porque sino no anda
         layerMask = ~layerMask;
+        //Aca defino a quienes puedo matar con un raycast y a quienes no
+		List<string> _listaDePuedoMatar = new List<string>(){ "Elizabeth", "Policia", "Otros" };
 
 		//Este es el hitaso ...
         RaycastHit hit;
-        
-		//Aca defino a quienes puedo matar con un raycast y a quienes no
-		List<string> _listaDePuedoMatar = new List<string>(){ "Elizabeth", "Policia", "Otros" };
-
-		//Si apretas click izquierdo del mouse y mantenes apretado ...
+    	//Si apretas click izquierdo del mouse y mantenes apretado ...
         if (Input.GetMouseButton(0)){
 
 			// Lanza un raycast ....
-			if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+			if (Physics.Raycast(pistola.transform.position, pistola.transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
         	{
 				//Si ese raycast no pega en el infinito (wooooooo) lo dibuja con color amarillo en la pantalla.
-	            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+	            Debug.DrawRay(pistola.transform.position, pistola.transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
 				
 				//Capturo el game object contra el que impacta el raycast.
 				GameObject _objetoRayCasteado = hit.collider.gameObject;
@@ -48,6 +69,8 @@ public class ThomasShoot : MonoBehaviour
         	}
 
 		}
-		
     }
+
+	
+
 }	
