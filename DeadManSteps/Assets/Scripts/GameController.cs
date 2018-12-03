64dd3a01ce855f2a1d5347f93e471749bd08f6a3
Federico Sanches <federicohernansanches@gmail.com>
2018-12-02 23:19:11 -0300

    #18 Dialogos
    Se agrego tutorial y dialogos cuando se agarra el celu

---
+++ DeadManSteps/Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+	
+	public static bool hasPhone;
+	[SerializeField]
+	private DialogueTrigger[] dialogueList;
+
+	// Use this for initialization
+	void Start () {
+		dialogueList[0].TriggerDialogue();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(hasPhone == true){
+			dialogueList[1].TriggerDialogue();
+		}
+		
+	}
+
+}
--- DeadManSteps/Assets/Scripts/pickUpItems.cs
+++ DeadManSteps/Assets/Scripts/pickUpItems.cs
@@ -2,18 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class pickUpItems : MonoBehaviour 
-{
+public class pickUpItems : MonoBehaviour {
 
-	private void OnTriggerStay(Collider other)
-	{
-		if (other.tag == "Player") 
-		{
-			if (Input.GetKey (KeyCode.E)) 
-			{
-				
-				Destroy (this.gameObject);	
+	private void OnTriggerStay(Collider other){
+		if (other.tag == "Player") {
+			if (Input.GetKey (KeyCode.E)) {
+				this.GetComponent<DialogueTrigger>().TriggerDialogue();
+				//GameController.hasPhone = true;		
+				//Destroy(this.gameObject);
 			} 	
 		} 
 	}
+
 }
