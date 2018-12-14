9382c229ad84b17fcb0032e48a0ef9c9f77f383b
martingaleano <galeanomarting@gmail.com>
2018-12-13 23:20:34 -0300

    Agrego transformador electrico

---
+++ DeadManSteps/Assets/Scripts/Destructible.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructible : MonoBehaviour {
+
+	public GameObject destroyedVersion;
+
+	void Update () {
+        if (Input.GetKeyDown("space")) {
+	        Instantiate(destroyedVersion, transform.position, transform.rotation);
+	        GameObject warning = GameObject.Find ("warning");
+	        Destroy(warning.gameObject);
+			Destroy(gameObject);
+        }
+    }
+
+
+}
